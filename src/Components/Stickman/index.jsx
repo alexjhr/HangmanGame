071943a661198db1e764d4883b12cf3f657cd4f0
alexjhr <96698651+alexjhr@users.noreply.github.com>
2022-02-12 21:50:55 -0400

    Fix vite assets

--- src/Components/Stickman/index.jsx
+++ src/Components/Stickman/index.jsx
@@ -1,7 +1,7 @@
 import {stickman, container} from './index.module.css'
 
 export default function Stickman({mistakes} = props) {
-	const imagePath = `/assets/game${mistakes}.png`; 
+	const imagePath = `./assets/game${mistakes}.png`; 
 
 	return <div className={container}>
 		<img src={imagePath} className={stickman} alt="Game stickman base" />
