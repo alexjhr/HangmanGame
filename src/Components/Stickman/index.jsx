88aeb0325194bbdf2839f06498f28e31cc607dff
alexjhr <96698651+alexjhr@users.noreply.github.com>
2022-02-14 13:48:34 -0400

    Ah?

--- src/Components/Stickman/index.jsx
+++ src/Components/Stickman/index.jsx
@@ -1,9 +1,9 @@
 import {stickman, container} from './index.module.css'
 
 export default function Stickman({mistakes} = props) {
-	const imagePath = `./assets/game${mistakes}.png`; 
+	const imgUrl = `./assets/game${mistakes}.png`;
 
 	return <div className={container}>
-		<img src={imagePath} className={stickman} alt="Game stickman base" />
+		<img src={imgUrl} className={stickman} alt="Game stickman base" />
 	</div>
 }
