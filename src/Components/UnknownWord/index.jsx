3fd0b218904a427d53e8b3505be19cf6d9059130
alexjhr <96698651+alexjhr@users.noreply.github.com>
2022-02-18 08:52:37 -0400

    Fix each child and className

--- src/Components/UnknownWord/index.jsx
+++ src/Components/UnknownWord/index.jsx
@@ -1,14 +1,14 @@
 import classes from './index.module.css'
 
-export default function UnknownWord({keyword, input} = props) {
-	const keywordCodified = keyword.split('').map((letter) => {
+export default function UnknownWord({keyword = '', input} = props) {
+	const keywordCodified = keyword.split('').map((letter, index) => {
 		const inKeyword = input.indexOf(letter) !== -1;
 		const unknownLetter = inKeyword ? letter : '';
 
-		return <div className={classes.letter} data-decodified={unknownLetter} data-actived={inKeyword}>_</div>
-	});
+		return <div key={index} className={classes.letter} data-letter={unknownLetter} data-actived={inKeyword}>_</div>
+	})
 
-	return <section class={classes.container}>
+	return <section className={classes.container}>
 		{keywordCodified}
 	</section>
 }
