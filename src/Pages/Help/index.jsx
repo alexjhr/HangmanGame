e6ecdacbe27e9e38248aa70094385be7815dd5a6
alexjhr <alexisjhr2004@gmail.com>
2023-02-19 18:04:42 -0400

    Fix parser html in help page

--- src/Pages/Help/index.jsx
+++ src/Pages/Help/index.jsx
@@ -1,18 +1,19 @@
-
-import { useContext } from 'react'
-import classes from './index.module.css'
-import BackButton from '../../Components/BackButton'
-import SettingsContext from '../../Context/SettingsContext'
-import ReactHtmlParser from 'react-html-parser';
+import { useContext } from "react";
+import classes from "./index.module.css";
+import BackButton from "../../Components/BackButton";
+import SettingsContext from "../../Context/SettingsContext";
+import parse from "html-react-parser";
 
 export default function Help() {
 	const { dictionary } = useContext(SettingsContext);
 
-	return <>
-		<BackButton />
+	return (
+		<>
+			<BackButton />
 
-		<div className={classes.info}>
-			{ReactHtmlParser(dictionary.game_help_info)}
-		</div>
-   </>
+			<div className={classes.info}>
+				{parse(dictionary.game_help_info)}
+			</div>
+		</>
+	);
 }
